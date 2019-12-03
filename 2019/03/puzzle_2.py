992dd0dd566d499db3ae2930995d6174f9b4cbc5
Julian Naydichev <git@julian.appwarden.com>
2019-12-03 08:19:26 +0100

    2019, day 3. this was fun.

---
+++ 2019/03/puzzle_2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+def main(wires):
+    wire_map = defaultdict(lambda: [set()])
+
+    i = 0
+    for wire in wires:
+        draw_wire(wire, i, wire_map)
+        i += 1
+
+    intersections = filter(lambda v: v is not None, [key if len(value[0]) == 2 else None for key, value in wire_map.items()])
+    min_distance = None
+    min_location = None
+
+    for cross in intersections:
+        distance = count_steps(cross, wire_map)
+
+        if min_distance is None or distance < min_distance:
+            min_distance = distance
+            min_location = cross
+
+    print(f"min distance is {min_distance} at location {min_location}")
+
+
+def count_steps(cross, wire_map):
+    wires = wire_map[cross][1:]
+    if len(wires) > 2:
+        print(wires)
+
+    return sum([wire[1] for wire in wires])
+
+DIRECTIONS = {
+    'L': lambda location: (location[0] + 1, location[1]),
+    'R': lambda location: (location[0] - 1, location[1]),
+    'U': lambda location: (location[0], location[1] + 1),
+    'D': lambda location: (location[0], location[1] - 1)
+}
+
+def draw_wire(wire, wire_num, wire_map):
+    location = (0, 0)
+    steps = 1
+    for part in wire:
+        direction = DIRECTIONS[part[:1]]
+        distance = int(part[1:])
+
+        for i in range(distance):
+            location = direction(location)
+            wire_map[location][0].add(wire_num)
+            wire_map[location].append((wire_num, steps))
+            steps += 1
+
+if __name__ == "__main__":
+    with open("wires.pi") as f:
+        main([line.rstrip().split(",") for line in f])
