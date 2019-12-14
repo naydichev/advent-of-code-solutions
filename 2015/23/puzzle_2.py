e2ee039c97f60411f7653ca66dad08e361d884f6
Julian Naydichev <git@julian.appwarden.com>
2019-12-14 17:37:22 +0100

    day 22 and 23, 2015; first one took a while... basically dfs. second one is a simpler intcode. my favorite 🤩

---
+++ 2015/23/puzzle_2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+
+def main(program):
+    i = 0
+    registers = dict(a=1, b=0)
+    while i < len(program):
+        instruction, arguments = program[i]
+        ip_changed = False
+
+        print(f"{i:03} - [A: {registers['a']:05}; B: {registers['b']:05}] - {instruction} {arguments}")
+        if instruction == "hlf":
+            registers[arguments] //= 2
+        elif instruction == "tpl":
+            registers[arguments] *= 3
+        elif instruction == "inc":
+            registers[arguments] += 1
+        elif instruction == "jmp":
+            i += int(arguments)
+            ip_changed = True
+        elif instruction == "jie":
+            r, offset = arguments.split(", ")
+            if registers[r] % 2 == 0:
+                i += int(offset)
+                ip_changed = True
+        elif instruction == "jio":
+            r, offset = arguments.split(", ")
+            if registers[r] == 1:
+                i += int(offset)
+                ip_changed = True
+
+        if not ip_changed:
+            i += 1
+
+    print(registers)
+
+
+if __name__ == "__main__":
+    with open("program.pi") as f:
+        main([(i[:3], i[4:]) for i in f.read().strip().split("\n")])
