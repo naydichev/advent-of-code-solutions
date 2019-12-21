8bede55e8b8f2191731c9360e69198a7412a3be4
Julian Naydichev <489233+naydichev@users.noreply.github.com>
2019-12-21 08:29:31 +0100

    day 21, 2019 - silly little puzzle

---
+++ 2019/21/puzzle_2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from functools import partial
+
+PROG = "program"
+PROC = "processing"
+OUTP = "output"
+
+
+def producer(data):
+    if not len(data[PROC]):
+        data[PROC].extend(list(data[PROG].pop(0)))
+        data[PROC].append("\n")
+
+    return ord(data[PROC].pop(0))
+
+
+def consumer(data, value):
+    if value != 10:
+        data[OUTP].append(value)
+    else:
+        line = [chr(x) for x in data[OUTP]]
+        print("".join(line))
+        data[OUTP].clear()
+
+
+def main(instructions):
+    data = {OUTP: [], PROG: [], PROC: []}
+    data[PROG] = generate_program()
+    execute_program(instructions.copy(), partial(producer, data), partial(consumer, data), False).join()
+    print(data[OUTP])
+
+
+def generate_program():
+    return [
+        "OR A J",
+        "AND B J",
+        "AND C J",
+        "NOT J J",
+        "AND D J",
+        "OR E T",
+        "OR H T",
+        "AND T J",
+        "RUN"
+    ]
+
+if __name__ == "__main__":
+    from sys import path
+    from os.path import dirname as dir
+
+    path.append(dir(path[0]))
+    __package__ = "aoc.19"
+    from intcode import execute_program
+
+    with open("springdroid.pi") as f:
+        main(list(map(int, f.read().strip().split(","))))
