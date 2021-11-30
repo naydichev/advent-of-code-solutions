361e89704366fcb590d5a5b3b2d5e24350ccc912
Julian Naydichev <julian@thinkreservations.com>
2021-11-30 10:01:13 +0100

    Adding an option to also open the URL

--- getinput.py
+++ getinput.py
@@ -22,14 +22,19 @@ def main(args):
 
     requests.utils.add_dict_to_cookiejar(session.cookies, dict(session=args.session_token))
 
+    event_url = f"https://adventofcode.com/{args.year}/day/{args.day}"
+
     print(f"Getting todays puzzle input")
-    r = session.get(f"https://adventofcode.com/{args.year}/day/{args.day}/input", stream=True)
+    r = session.get(f"{event_url}/input", stream=True)
     r.raise_for_status()
 
     print(f"Saving to {destination}/input.aoc")
     with open(f"{destination}/input.aoc", "wb") as f:
         for content in r.iter_content():
             f.write(content)
+
+    if args.open:
+        os.system(f"open {event_url}")
 
 
 def parse_args():
@@ -38,6 +43,7 @@ def parse_args():
     parser.add_argument("--session-token", help="Session token override.")
     parser.add_argument("--day", "-d", type=int, help="Which day to get the input for, defaults to 'today'")
     parser.add_argument("--year", "-y", type=int, help="Which year to get the input for, defaults to 'this year'")
+    parser.add_argument("--open", "-o", action="store_true", help="Whether or not to open the URL")
 
     args = parser.parse_args()
 
